295574d6e1c9f0dabed92544ac4f4bd0af4c1edf
Avalos Ortega <avaloan@emea.corpdir.net>
2020-11-18 21:15:28 +0100

    feat: messari.py update

--- tradingBot/mktData/messari.py
+++ tradingBot/mktData/messari.py
@@ -14,7 +14,7 @@ response = json.loads(response.text)
 
 #historic candles
 
-url = "https://data.messari.io/api/v1/assets/bitcoin/metrics/price/time-series?start=2020-01-01&end=2020-02-01&interval=1d"
+url = "https://data.messari.io/api/v1/markets/binance-btc-usdt/metrics/price/time-series?start=2020-01-01&end=2020-02-01&interval=1d"
 
 response1 = requests.get(url)
 response1 = json.loads(response1.text)
