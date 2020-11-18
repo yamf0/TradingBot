006cee4df3fcc6f76cbff29df8c98058282e1815
Avalos Ortega <avaloan@emea.corpdir.net>
2020-11-18 21:10:04 +0100

    feat: mesari json

--- tradingBot/mktData/messari.py
+++ tradingBot/mktData/messari.py
@@ -5,16 +5,16 @@ import json
 from time import sleep, ctime
 
 
+#current data
 
-# if you encounter a "year is out of range" error the timestamp
-# may be in milliseconds, try `ts /= 1000` in that case
+url = "https://data.messari.io/api/v1/assets/eth/metrics/market-data"
 
-
-##GET INFO OF ALL COINS
-url = "https://data.messari.io/api/v2/assets"
-
-params = {"search" : "ethereum", "fields" : "id,slug,symbol,metrics/market_data/price_usd", "marketKey" : "binance", "limit" : 1}
-
-response = requests.get(url,  params = params)
-print(response.url)
+response = requests.get(url)
 response = json.loads(response.text)
+
+#historic candles
+
+url = "https://data.messari.io/api/v1/assets/bitcoin/metrics/price/time-series?start=2020-01-01&end=2020-02-01&interval=1d"
+
+response1 = requests.get(url)
+response1 = json.loads(response1.text)
