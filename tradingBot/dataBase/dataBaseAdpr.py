9f80a47eb426ad01047eeaaf9b581e57399e7f6a
Hugo Armando Zepeda Ruiz <hugozepeda6@gmail.com>
2021-01-15 19:19:17 +0100

    feat(dataBaseAdpr): DB Update, missing testing

--- tradingBot/dataBase/dataBaseAdpr.py
+++ tradingBot/dataBase/dataBaseAdpr.py
@@ -15,7 +15,7 @@ import ast
 
 SCOPES = ['https://www.googleapis.com/auth/drive']
 
-class dataBaseAdpr():
+class dataBase():
     ## Data Base class
     # @class dataBaseAdpr 
     # @brief This class handles the update of the data base in Google Drive
@@ -23,9 +23,10 @@ class dataBaseAdpr():
     def __init__(self):
         ## @fn __init__ 
         # @brief  Class constructor in charge of autentication process for the Google Drive API
+        self.path = os.path.dirname(os.path.abspath(__file__))
         creds = None
-        if os.path.exists('token.pickle'):
-            with open('token.pickle', 'rb') as token:
+        if os.path.exists(self.path + '/token.pickle'):
+            with open(self.path + '/token.pickle', 'rb') as token:
                 creds = pickle.load(token)
                 print('Taking credentials')
         if not creds or not creds.valid:
@@ -33,14 +34,14 @@ class dataBaseAdpr():
                 creds.refresh(Request())
             else:
                 flow = InstalledAppFlow.from_client_secrets_file(
-                    'credentials.json', SCOPES)
+                    self.path + '/credentials.json', SCOPES)
                 print('Creating credentials')
                 creds = flow.run_local_server(port=0)
-            with open('token.pickle', 'wb') as token:
+            with open(self.path + '/token.pickle', 'wb') as token:
                 pickle.dump(creds, token)
                 print('Credentials safe for next run')
         self.drive_service = build('drive', 'v3', credentials=creds)
-        
+
         '''
         ###TESTING BELOW###
         searchKey = 'OCHL'
@@ -55,31 +56,47 @@ class dataBaseAdpr():
     def updateFiles(self):
         ## @fn updateFiles 
         # @brief public method which handles the update of all the files to the data base on the cloud
-        with open('jsonFolder/documentIDs.json','r') as f:
-            dictIds = json.load(f)
+        with open(self.path + '/jsonFolder/documentIDs.json','r') as f:
+            self.dictIds = json.load(f)
             for key in dictIds:
-                pathFile = 'jsonFolder/' + key
-                media = MediaFileUpload(pathFile,
+                pathFile = self.path + '/jsonFolder/' + key
+                self.media = MediaFileUpload(pathFile,
                                         mimetype='application/json',
                                         resumable=True)
-                file = self.drive_service.files().update(
-                                                    media_body=media,
-                                                    fileId=dictIds[key]).execute()
+                self._updateFilesCall()
+                
+    def _updateFilesCall(self):
+        file = self.drive_service.files().update(media_body=self.media,
+                                                fileId=self.dictIds[key]).execute()
+        for key in file:
+            if(key == 'id'): return True
 
     def downloadFiles(self):
         ## @fn downloadFiles 
         # @brief public method which handles the download of all the files to the local data base of the developer
-        with open('jsonFolder/documentIDs.json','r') as f:
+        with open(self.path + '/jsonFolder/documentIDs.json','r') as f:
             dictIds = json.load(f)
             for key in dictIds:
-                pathFile = 'jsonFolder/' + key
-                content = self.drive_service.files().get_media(fileId=dictIds[key]).execute()
-                content =  content.decode('utf-8')
-                content = ast.literal_eval(content)
-                with open(pathFile,'w') as f:
-                    json.dump(content,f)
-    '''
-    def updateFolderIds(self):
+                pathFile = self.path + '/jsonFolder/' + key
+                self._downloadFiles(dictIds, key, pathFile)
+
+    def _downloadFiles(self, dictIds, key, pathFile):
+        file = self.drive_service.files().get_media(fileId=dictIds[key]).execute()
+        for key in file:
+            if(key == 'id'): value = True
+            self._decodingDownloadFile(file)
+
+    def _decodingDonwloadFile(self, file):
+        content =  file.decode('utf-8')
+        content = ast.literal_eval(content)
+        with open(pathFile,'w') as f:
+            json.dump(content,f)
+            return True
+    
+    def updateFolderIds(self, drive_service):
+        ## @fn updateFolderIds 
+        # @brief public method which handles the manual update of folders in the data base Google Drive API
+        # TO-DO: implement a function which updates the file automatically and not manually. 
         page_token = None
         while True:
             response = drive_service.files().list(q="mimeType='application/vnd.google-apps.folder'",
@@ -92,7 +109,7 @@ class dataBaseAdpr():
             page_token = response.get('nextPageToken', None)
             if page_token is None:
                 break
-    '''
+    
     def createUpdateFile(self, f_nameFile, f_pathFile, f_folderID):
         ## @fn createUpdateFile 
         # @brief public method for the creation of a new file
@@ -108,15 +125,19 @@ class dataBaseAdpr():
                                 resumable=True)
         file = self.drive_service.files().create(body=file_metadata,
                                             media_body=media,
-                                            fields='id').execute()        
+                                            fields='id').execute()  
+
+        ####AQUI YA LO SUBISTE###
+        # PUEDO HACER EL TEST CON .GET       
         newID = file.get('id')
         newKey = {f_nameFile : newID}
-        with open('jsonFolder/documentIDs.json','r') as f:
+        with open(self.path + '/jsonFolder/documentIDs.json','r') as f:
             dictIds = json.load(f)
             dictIds.update(newKey)
-        with open('jsonFolder/documentIDs.json','w') as f:
+        with open(self.path + '/jsonFolder/documentIDs.json','w') as f:
             json.dump(dictIds,f)
-
+    
+    ### PREGUNTAR A YAEL Q OPINA DE ESTA FUNCION, COMO LA TESTIARIA EL 
     def keySearchValue(self, f_searchKeys, f_pathFile):
         ## @fn keySearchValue 
         # @brief public method to look up for a folderID
@@ -129,8 +150,8 @@ class dataBaseAdpr():
                 folderID = self.__keySearch(f_searchKeys, dictIds[key])
                 return folderID
 
-    
-    def __keySearch(self, f_searchKeys, f_dictIds):
+    ### PREGUNTAR A YAEL Q OPINA DE ESTA FUNCION, COMO LA TESTIARIA EL 
+    def _keySearch(self, f_searchKeys, f_dictIds):
         ## @fn keySearchValue 
         # @brief private method to do recursion on a dictionary
         # @param f_searchKeys Looked up key
@@ -145,7 +166,7 @@ class dataBaseAdpr():
                 return (f_dictIds[key])
 
 def main():
-    workAround = dataBaseAdpr()
+    workAround = dataBase()
 
 
 if __name__ == '__main__':
